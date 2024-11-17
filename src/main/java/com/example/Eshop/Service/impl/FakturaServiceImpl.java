20d042a2e40c831e85fd7db9b402d222d2e00a43
Michaeel33 <fathalipourmichael@gmail.comm>
2024-11-17 11:27:04 +0100

    Refaktor - presunutie logiky z DAO na servisnu vrstvu

--- src/main/java/com/example/Eshop/Service/impl/FakturaServiceImpl.java
+++ src/main/java/com/example/Eshop/Service/impl/FakturaServiceImpl.java
@@ -1,23 +1,49 @@
 package com.example.Eshop.Service.impl;
 
 import com.example.Eshop.Dao.FakturaDao;
-import com.example.Eshop.Dto.FakturaDto;
+import com.example.Eshop.Dao.Impl.ItemsDao;
+import com.example.Eshop.Dao.OrdersDao;
+import com.example.Eshop.Dao.PersonalDataDao;
+import com.example.Eshop.Dao.PersonalDocumentsDao;
+import com.example.Eshop.Dto.*;
 import com.example.Eshop.Service.FakturaService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class FakturaServiceImpl  implements FakturaService {
 
     @Autowired
-    private final FakturaDao fakturaDao;
-
+    private final PersonalDataDao personalDataDao;
+    @Autowired
+    private final PersonalDocumentsDao personalDocumentsDao;
+    @Autowired
+    private final OrdersDao ordersDao;
+    @Autowired
+    private final ItemsDao itemsDao;
 
     @Override
     public FakturaDto getFakturaHistory(long perId) {
-        return fakturaDao.getFaktura(perId);
+        FakturaDto fakturaDto = new FakturaDto();
+        PersonalData personalDataDto = personalDataDao.getPersonalData(perId);
+        PersonalDocuments personalDocuments = personalDocumentsDao.getPersonalDoc(perId);
+        List<Orders> ordersList = ordersDao.getOrdersForPerId(perId);
+
+        for (Orders order : ordersList) {
+            List<Items> itemList = itemsDao.getItemsForOrder(order.getOrderNumber());
+            order.setOrderedItems(itemList);
+        }
+
+        fakturaDto.setPersId(perId);
+        fakturaDto.setPersonalData(personalDataDto);
+        fakturaDto.setPersonalDocuments(personalDocuments);
+        fakturaDto.setOrdersList(ordersList);
+
+        return fakturaDto;
     }
 }
